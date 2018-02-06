04c1e929cf1e0f29c718590bffdca9e2ab00e07c
E Kawashima <e-kwsm@users.noreply.github.com>
2018-02-06 21:45:47 +0900

    implement parallel_reducible_histogram

---
+++ parallel_reducible_histogram.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+#include <array>
+#include <tbb/blocked_range.h>
+#include "parallel_reducible_histogram.h"
+
+int main() {
+  constexpr size_t n = 1'000'000;
+  using array_t = std::array<int, n>;
+  array_t a;
+  for (size_t i = 0; i < n; i++)
+    a[i] = i;
+
+  for (auto [k, v] : parallel_reduce_histogram<size_t, unsigned>(a, [](auto k) {
+         constexpr auto bin = n / 10;
+         return (k / bin) * bin;
+       }))
+    std::cout << k << '\t' << v << '\n';
+}
---
+++ parallel_reducible_histogram.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <map>
+#include <tbb/blocked_range.h>
+#include <tbb/parallel_reduce.h>
+
+template<typename Key, typename Value, typename C, typename F>
+class ParallelReducibleHistogram {
+public:
+  ParallelReducibleHistogram(const C& container, F f)
+      : container{container}, f{f} {}
+  ParallelReducibleHistogram(const ParallelReducibleHistogram& other, tbb::split)
+      : container(other.container), f{other.f} {}
+
+  template<typename R> void operator()(const R& r) {
+    auto tmp = histogram;
+    for (auto i = r.begin(); i != r.end(); ++i)
+      ++tmp[f(*i)];
+    histogram = tmp;
+  }
+
+  void join(const ParallelReducibleHistogram& other) {
+    for (const auto& [k, v] : other.histogram)
+      histogram[k] += v;
+  }
+  auto get() const noexcept { return histogram; }
+
+private:
+  const C& container;
+  F f;
+
+  std::map<Key, Value> histogram;
+};
+
+template<typename Key, typename Value, typename C, typename F>
+auto parallel_reduce_histogram(const C& container, F f) -> typename std::enable_if_t<
+    std::is_same<typename std::iterator_traits<typename C::iterator>::iterator_category,
+                 std::random_access_iterator_tag>::value,
+    std::map<Key, Value>> {
+  ParallelReducibleHistogram<Key, Value, C, F> hist{container, f};
+  tbb::parallel_reduce(
+      tbb::blocked_range<typename C::const_iterator>{container.cbegin(), container.cend()}, hist);
+  return hist.get();
+}
